651aa6ae740b7ccebd97bb19e7de2b13ad979cf5
ajpahl1008 <ajpahl1008@gmail.com>
2014-07-05 19:53:03 -0400

    Clearing out tests which should be in the Web Service code.

--- megaevent-ui/src/test/java/com/pahlsoft/megaevent/ws/test/TestClient.java
+++ megaevent-ui/src/test/java/com/pahlsoft/megaevent/ws/test/TestClient.java
@@ -1,38 +1,9 @@
 package com.pahlsoft.megaevent.ws.test;
 
-import static org.junit.Assert.*;
 
-import com.pahlsoft.megaevent.ws.generated.Event;
-import org.junit.Before;
-import org.junit.Test;
-
-import com.pahlsoft.megaevent.executor.EventClientImpl;
-
-import java.util.UUID;
 
 
 public class TestClient {
 
-	EventClientImpl eci;
-    Event event;
-
-	@Before
-	public void setup() {
-		eci = new EventClientImpl();
-        event = new Event();
-	}
-
-    @Test
-    public void addNewEventShouldPass() throws Exception {
-        event.setDescription("UnitTestDescription");
-        event.setEventStatus("ACTIVE");
-        event.setTitle("TestEvent: " + UUID.randomUUID());
-        assertEquals(1, eci.addEvent(event));
-    }
-
-	@Test
-	public void test() throws Exception {
-		assertEquals(" The Very First Event",eci.getEvent(2).getDescription());
-	}
 
 }
